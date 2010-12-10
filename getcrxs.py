e2c75ba565cf0cb76a7963d3ad4ab6b77a43b766
mhanson@gmail.com <michaelhanson@host-6-20.mv.mozilla.com>
2010-12-10 11:10:10 -0800

    Refactoring

--- getcrxs.py
+++ getcrxs.py
@@ -4,8 +4,24 @@ from urlparse import urlparse
 import urllib
 import string
 from BeautifulSoup import BeautifulSoup
+from Queue import Queue
 
-def getCats():
+class ChromeCrawler(object):
+  def __init__(self):
+    self.pageQueue = Queue()
+    self.pageQueue.put(("root", "https://chrome.google.com/webstore"))
+    
+  def step(self):
+    if not self.pageQueue.empty():
+      page = self.pageQueue.get()
+      if page[0] == "root":
+        self.processRoot(page[1])
+      elif page[1] == "directory":
+        self.processDirectory(page[1])
+      elif page[2] == "app":
+        self.processApp(page[2])
+  
+  def processRoot(self, url):
     u = urllib.urlopen("https://chrome.google.com/webstore")
     html = u.read()
     soup = BeautifulSoup(html)
@@ -14,7 +30,16 @@ def getCats():
         loc = urlparse(x['href'])
         if string.find(loc['query'], '=app'):
             l.append('https://chrome.google.com/webstore/list/most_popular?' + loc['query'])
+
+    # put 'em in the queue
     return l
+  
+  def processDirectory(self, url):
+    pass
+
+  def processApp(self, url):
+    pass
+    
 
 for i in getCats():
     print i
