0b02388c2705a4f91bb900027d82b9a78d3773f9
mhanson@gmail.com <michaelhanson@host-6-20.mv.mozilla.com>
2010-12-15 12:03:17 -0800

    Reworked to use URLs for install(); call convertcrx as needed; move install.html to splash dir.

--- server.py
+++ server.py
@@ -19,59 +19,6 @@ CRX_DOWNLOAD_BASE = "https://clients2.google.com/service/update2/crx?response=re
 MANIFEST_STORAGE_DIR = ".server_manifests"
 CRX_DUMP_DIR = ".server_crx_dump"
 
-class InstallCRXHandler(tornado.web.RequestHandler):
-  def get(self):
-    url = self.get_argument("url", None)
-    if not url:
-      self.render("error.html", error="Missing required 'url' parameter")
-      return
-    
-    logging.debug("installcrx url is %s" % url)
-    parsed = urlparse.urlparse(url)
-    path = parsed.path
-    hashCheck = path.rfind('#')
-    if hashCheck > 0:
-      path = path[:hashCheck]
-      logging.debug("removed hash %s" % path)
-    theID = path[path.rfind("/")+1:]
-    logging.debug("id is %s" % theID)
-
-    if not self.manifest_exists(theID):
-      self.fetch_crx(theID)
-      if not self.manifest_exists(theID):
-        self.render("error.html", error="Unable to retrieve application manifest.")
-        return
-    
-    dataFD = open(self.manifest_storage_path(theID), "r")
-    manifest = dataFD.read()
-    dataFD.close()
-    self.render("install.html", manifest=manifest, return_to=url)
-
-  def manifest_storage_path(self, theID):
-    return "%s/%s/%s" % (os.environ["HOME"], MANIFEST_STORAGE_DIR, theID)
-
-  def manifest_exists(self, theID):
-    return os.path.exists(self.manifest_storage_path(theID))
-
-  def fetch_crx(self, theID):
-    downloadURL = CRX_DOWNLOAD_BASE % theID
-
-    # Go get it!
-    downloadConn = urllib.urlopen(downloadURL)
-    downloadBytes = downloadConn.read()
-    if SAVE_CRX_DUMPS:
-      dumpFile = open("%s/%s/%s.crx" % (os.environ["HOME"], CRX_DUMP_DIR, theID), "w")
-      dumpFile.write(downloadBytes)
-      dumpFile.close()
-    
-    manifest = CRXConverter().convert(StringIO(downloadBytes))
-    outputFile = open(self.manifest_storage_path(theID), "w")
-    outputFile.write(json.dumps(manifest))
-    outputFile.close()
-
-
-
-
 class ConvertCRXHandler(tornado.web.RequestHandler):
   def manifest_storage_path(self, theID):
     return "%s/%s/%s" % (os.environ["HOME"], MANIFEST_STORAGE_DIR, theID)
@@ -141,7 +88,6 @@ settings = {
 }
 
 application = tornado.web.Application([
-    (r"/installcrx", InstallCRXHandler),
     (r"/convertcrx", ConvertCRXHandler)
 ], **settings)
 
